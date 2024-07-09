35b0952e7c54e48e8d2d4cb4cd11fc0f04947acd
Mohd Shamsher <mohdshamsher365@gmail.com>
2024-07-09 19:54:19 +0530

    zero-triplet-updated

--- Array/f2-zero-triplets.js
+++ Array/f2-zero-triplets.js
@@ -4,8 +4,8 @@ function zeroTriplets(arr){
     let found = false;
 
     for(let i=0; i<arr.length-2; i++){
-        for(let j=0; j<arr.length-1; j++){
-            for(let k=0; k<arr.length; k++){
+        for(let j=i+1; j<arr.length-1; j++){
+            for(let k=j+1; k<arr.length; k++){
                 if(arr[i]+arr[j]+arr[k]===0){
                     console.log(arr[i],arr[j], arr[k]);
                     found = true;
